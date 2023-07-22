c086f9337174a4a152fb0d033dce333323551a15
wshena <81917362+wshena@users.noreply.github.com>
2023-07-22 20:22:06 +0700

    Update index.js

--- src/content/index.js
+++ src/content/index.js
@@ -3,15 +3,15 @@ export const NavLink = ['home', 'shop', 'about', 'contact']
 export const Slide = [
 	{
 		id:1,
-		ImageDesktop: 'src/assets/desktop-image-hero-1.jpg',
-		ImageMobile: 'src/assets/mobile-image-hero-1.jpg', 
+		ImageDesktop: '../assets/desktop-image-hero-1.jpg',
+		ImageMobile: '../assets/mobile-image-hero-1.jpg', 
 		title: 'Discover inovative ways to decorate',
 		paragraph: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
 	},
 	{
 		id:2,
-		ImageDesktop: 'src/assets/desktop-image-hero-2.jpg', 
-		ImageMobile: 'src/assets/mobile-image-hero-2.jpg', 
+		ImageDesktop: '../assets/desktop-image-hero-2.jpg', 
+		ImageMobile: '../assets/mobile-image-hero-2.jpg', 
 		title: 'We are available all across the globe',
 		paragraph: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
 
