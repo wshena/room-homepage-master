744b785293d42e9d91fd7076b259021d784c06a0
wshena <81917362+wshena@users.noreply.github.com>
2023-07-22 20:33:44 +0700

    Update index.js

--- src/content/index.js
+++ src/content/index.js
@@ -1,25 +1,32 @@
+import Img1Desktop from '../assets/desktop-image-hero-1.jpg'
+import Img1Mobile from '../assets/mobile-image-hero-1.jpg'
+import Img2Desktop from '../assets/desktop-image-hero-2.jpg'
+import Img2Mobile from '../assets/mobile-image-hero-2.jpg'
+import Img3Desktop from '../assets/desktop-image-hero-3.jpg'
+import Img3Mobile from '../assets/mobile-image-hero-3.jpg'
+
 export const NavLink = ['home', 'shop', 'about', 'contact']
 
 export const Slide = [
 	{
 		id:1,
-		ImageDesktop: '../assets/desktop-image-hero-1.jpg',
-		ImageMobile: '../assets/mobile-image-hero-1.jpg', 
+		ImageDesktop: Img1Desktop,
+		ImageMobile: Img1Mobile, 
 		title: 'Discover inovative ways to decorate',
 		paragraph: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
 	},
 	{
 		id:2,
-		ImageDesktop: '../assets/desktop-image-hero-2.jpg', 
-		ImageMobile: '../assets/mobile-image-hero-2.jpg', 
+		ImageDesktop: Img2Desktop,
+		ImageMobile: Img2Mobile, 
 		title: 'We are available all across the globe',
 		paragraph: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
 
 	},
 	{
 		id:3, 
-		ImageDesktop: 'src/assets/desktop-image-hero-3.jpg', 
-		ImageMobile: 'src/assets/mobile-image-hero-3.jpg', 
+		ImageDesktop: Img3Desktop,
+		ImageMobile: Img3Mobile, 
 		title: 'Manufactured with the best materials',
 		paragraph: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
 	}
